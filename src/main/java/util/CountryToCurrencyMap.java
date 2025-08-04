cb18a4911f504ceeeb6967be13750aa67b3a7e8b
Juan Vidana <jjvidaher@hotmail.com>
2025-08-04 16:50:36 -0600

    feat: structuring project

---
+++ src/main/java/util/CountryToCurrencyMap.java
@@ -0,0 +1,47 @@
+package src.main.java.util;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class CountryToCurrencyMap {
+    private static final Map<String, String> countryCurrencyMap = new HashMap<>();
+
+    static {
+        countryCurrencyMap.put("mexico", "MXN");
+        countryCurrencyMap.put("estados unidos", "USD");
+        countryCurrencyMap.put("argentina", "ARS");
+        countryCurrencyMap.put("colombia", "COP");
+        countryCurrencyMap.put("chile", "CLP");
+        countryCurrencyMap.put("peru", "PEN");
+        countryCurrencyMap.put("espana", "EUR");
+        countryCurrencyMap.put("alemania", "EUR");
+        countryCurrencyMap.put("reino unido", "GBP");
+        countryCurrencyMap.put("japon", "JPY");
+        countryCurrencyMap.put("canada", "CAD");
+        countryCurrencyMap.put("brasil", "BRL");
+        countryCurrencyMap.put("venezuela", "VES");
+        countryCurrencyMap.put("uruguay", "UYU");
+        countryCurrencyMap.put("paraguay", "PYG");
+        countryCurrencyMap.put("bolivia", "BOB");
+        countryCurrencyMap.put("guatemala", "GTQ");
+        countryCurrencyMap.put("honduras", "HNL");
+        countryCurrencyMap.put("nicaragua", "NIO");
+        countryCurrencyMap.put("costa rica", "CRC");
+        countryCurrencyMap.put("panama", "PAB");
+        countryCurrencyMap.put("cuba", "CUP");
+        countryCurrencyMap.put("republica dominicana", "DOP");
+        countryCurrencyMap.put("el salvador", "USD");
+        countryCurrencyMap.put("ecuador", "USD");
+        countryCurrencyMap.put("suiza", "CHF");
+        countryCurrencyMap.put("sudafrica", "ZAR");
+        countryCurrencyMap.put("australia", "AUD");
+        countryCurrencyMap.put("china", "CNY");
+        countryCurrencyMap.put("india", "INR");
+        countryCurrencyMap.put("rusia", "RUB");
+        countryCurrencyMap.put("italia", "EUR");
+        countryCurrencyMap.put("francia", "EUR");
+        countryCurrencyMap.put("portugal", "EUR");
+    }
+
+
+}
